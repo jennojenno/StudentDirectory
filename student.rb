fb1cacfa2e6abcf82665cdbdf0eae6721739e112
Jen Lim <jennolim@gmail.com>
2013-03-27 14:19:11 -0400

    Add fields asking for hometown, github name, twitter, and fun fact

--- student.rb
+++ student.rb
@@ -1,9 +1,18 @@
+# Add attributes that you want to know about your classmates (don't be nosy...)
+# Finish the implementation so that you ask for all the attributes in your classes and save them in attr_accessors.
+# Refactor this code so that we can create a Student or an Instructor, based on the users imput (a bit like the Factory pattern we saw earlier...).
+# Make any other improvements you see fit
+# Go around the class and ask your classmates to complete the information provided
+# Check that you have a student_directory.yml file in your project folder.
+
+
 require 'rubygems'
 require 'yaml'
 
 class Person
   attr_accessor :name
   attr_accessor :email
+  attr_accessor :hometown
   attr_accessor :github_user
   attr_accessor :twitter
   attr_accessor :fun_fact
@@ -18,7 +27,7 @@ class Instructor < Person
 end
 
 @directory = ""
-puts "Student Directory, v0.0.1 by Dan Garland"
+puts "Student Directory, v0.0.2 by Jen Lim"
 print "Enter Student or Instructor, q to save and quit: "
 
 while ((input = gets.strip.chomp) != 'q') do
@@ -31,6 +40,14 @@ while ((input = gets.strip.chomp) != 'q') do
     person.name = gets.strip.chomp
     print "What is your email? "
     person.email = gets.strip.chomp
+    print "Where are you from? "
+    person.hometown = gets.strip.chomp
+    print "What's your GitHub username? "
+    person.github_user = gets.strip.chomp
+    print "What's your twitter username? "
+    person.twitter = gets.strip.chomp
+    print "Tell me a fun fact about yourself! "
+    person.fun_fact = gets.strip.chomp 
     
   when 'Instructor'
     person = Instructor.new
@@ -40,6 +57,14 @@ while ((input = gets.strip.chomp) != 'q') do
     person.email = gets.strip.chomp
     print "What sort of instructor are you? "
     person.type = gets.strip.chomp
+    print "Where are you from? "
+    person.hometown = gets.strip.chomp
+    print "What's your GitHub username? "
+    person.github_user = gets.strip.chomp
+    print "What's your twitter username? "
+    person.twitter = gets.strip.chomp
+    print "Tell me a fun fact about yourself! "
+    person.fun_fact = gets.strip.chomp 
   end
   
   # Append this to our yaml file
